a0d4184f351e338940a5fcc052cc599922faa006
christorubio <chrisrubio1997@gmail.com>
2022-04-06 20:40:28 -0500

    commit on 'main'

---
+++ iosapp/src/pages/IndexThree.vue
@@ -0,0 +1,72 @@
+<template>
+    
+    <q-page padding>
+
+<div style="width: 50%;">
+    <q-form action="/some-url" method='post'>
+    <h3>Name</h3>
+    <q-input filled name="name" float-label="Float Label" placeholder="name" v-model="text" />
+
+    <h3>Size</h3>
+    <q-btn-toggle filled name="size" :options="[
+    { label: 'Small', value: 'Small' },
+    { label: 'Medium', value: 'Medium' },
+    { label: 'Large', value: 'Large'}
+    ]" 
+    v-model="list"/>
+
+    <h3>SKU</h3>
+    <q-input filled name="sku" v-model="tex" />
+
+    <h2>Date Created</h2>
+    <q-date filled name="date" v-model="date" />
+    <h4>Submit Button</h4>
+    <q-btn type="submit" label="submit"/>
+    </q-form>
+
+
+</div>
+
+        <q-page-sticky position="right" style="width: 50%;">
+            <q-card>
+                <q-card-section>
+                    <pre> ({ $data })</pre>
+                </q-card-section>
+            </q-card>
+            <q-card-actions>
+                <q-btn @click="logger($data)"
+                label="logger" />
+            </q-card-actions>
+        </q-page-sticky>
+
+
+    </q-page>
+</template>
+
+<script>
+export default {
+
+        data () {
+            return {
+                name: 'Test',
+
+                size: 'Test1',
+
+                sku:'Test2',
+
+                date: null,
+            }
+        },
+    }
+
+    
+
+
+</script>
+
+<style>
+h2 {
+    margin-bottom: 12px;
+    margin-top: 120px;
+}
+</style>
---
+++ iosapp/src/pages/IndexTwo.vue
@@ -0,0 +1,61 @@
+<template>
+  
+  <q-page class="row items-center justify-evenly">
+   
+
+    <q-body>
+     <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input filled
+        
+        v-model="text"
+        label="Enter Work Order #"
+        
+      />
+
+        <q-input filled
+        
+        v-model="text"
+        label="Enter Product ID"
+        
+      />
+
+      <q-input filled
+        
+        v-model="name"
+        label="Enter Quantity"
+        
+      />
+
+      <q-input filled
+        
+        type="date"
+        v-model="date"
+        label=""
+        hint="Date Ordered"
+        
+        
+      />
+
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
+
+   
+    </q-body>
+   
+  </q-page>
+</template>
+
+
