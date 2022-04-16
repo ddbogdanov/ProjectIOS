e955420b655a58df48643f372bff787c7fb6e07f
ddbogdanov <dennis.d.bogdanov@gmail.com>
2022-04-16 04:07:00 -0500

    Remove finished todo

--- server/src/main/kotlin/com/exatech/ios/IosApplication.kt
+++ server/src/main/kotlin/com/exatech/ios/IosApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class IosApplication
-//TODO Get a product orders corresponding accessory orders
 fun main(args: Array<String>) {
     runApplication<IosApplication>(*args)
 }
